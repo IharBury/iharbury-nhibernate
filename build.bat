c913ee95b0b42e7abdd4a9e492087665528d67d7
Ihar Bury <Ihar.Bury@gmail.com>
2017-10-28 22:32:41 +0200

    Build script improved to avoid changing source code to release a new version

--- build.bat
+++ build.bat
@@ -1,2 +1,7 @@
 @echo off
+echo using System.Reflection; >IharBury.NHibernate\Properties\AssemblyVersion.cs
+echo [assembly: AssemblyVersion("%1.0")] >>IharBury.NHibernate\Properties\AssemblyVersion.cs
+echo [assembly: AssemblyFileVersion("%1.0")] >>IharBury.NHibernate\Properties\AssemblyVersion.cs
+echo [assembly: AssemblyInformationalVersion("%1")] >>IharBury.NHibernate\Properties\AssemblyVersion.cs
 nuget pack IharBury.NHibernate -Build -Prop Configuration=Release -Symbols
+echo. >IharBury.NHibernate\Properties\AssemblyVersion.cs
