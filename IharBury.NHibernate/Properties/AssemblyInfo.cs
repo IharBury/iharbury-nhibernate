54c78f1c97aa9664247ad207ad850fa1d2d783a1
Ihar Bury <Ihar.Bury@gmail.com>
2017-10-28 22:57:45 +0200

    Test project added

--- IharBury.NHibernate/Properties/AssemblyInfo.cs
+++ IharBury.NHibernate/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("IharBury.NHibernate")]
 [assembly: AssemblyDescription("Extends NHibernate.")]
 [assembly: AssemblyProduct("IharBury.NHibernate")]
 [assembly: AssemblyCopyright("Copyright © Ihar Bury 2017")]
+[assembly: InternalsVisibleTo("IharBury.NHibernate.Tests")]
--- build.bat
+++ build.bat
@@ -1,7 +1,17 @@
 @echo off
+nuget restore
+IF %ERRORLEVEL% NEQ 0 exit
+
 echo using System.Reflection; >IharBury.NHibernate\Properties\AssemblyVersion.cs
 echo [assembly: AssemblyVersion("%1.0")] >>IharBury.NHibernate\Properties\AssemblyVersion.cs
 echo [assembly: AssemblyFileVersion("%1.0")] >>IharBury.NHibernate\Properties\AssemblyVersion.cs
 echo [assembly: AssemblyInformationalVersion("%1")] >>IharBury.NHibernate\Properties\AssemblyVersion.cs
-nuget pack IharBury.NHibernate -Build -Prop Configuration=Release -Symbols
+msbuild IharBury.NHibernate.sln /property:Configuration=Release
+set msbuilderrorlevel=%ERRORLEVEL%
 echo. >IharBury.NHibernate\Properties\AssemblyVersion.cs
+IF %msbuilderrorlevel% NEQ 0 exit
+
+packages\xunit.runner.console.2.3.1\tools\net452\xunit.console.exe IharBury.NHibernate.Tests\bin\Release\IharBury.NHibernate.Tests.dll
+IF %ERRORLEVEL% NEQ 0 exit
+
+nuget pack IharBury.NHibernate -Prop Configuration=Release -Symbols
